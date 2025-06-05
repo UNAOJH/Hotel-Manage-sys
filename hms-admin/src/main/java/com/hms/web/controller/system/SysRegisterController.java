499f6aaaf5fbf60866727d27c3566871a1b196ba
UNAOJH <UNAOJH@outlook.com>
2025-06-05 21:58:35 +0800

    Initial commit 初始化提交

---
+++ hms-admin/src/main/java/com/hms/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.hms.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.hms.common.core.controller.BaseController;
+import com.hms.common.core.domain.AjaxResult;
+import com.hms.common.core.domain.model.RegisterBody;
+import com.hms.common.utils.StringUtils;
+import com.hms.framework.web.service.SysRegisterService;
+import com.hms.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
